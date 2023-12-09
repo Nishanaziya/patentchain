7e637d48c961a4544ff4088b14683f8c9df852af
Nisha Naziya <nishannp366@gmail.com>
2023-12-09 14:20:13 +0530

    changes to client app.js & app.css

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,35 @@
-// src/App.js
 import React, { useState } from "react";
 import "./App.css";
 
 function App() {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
+  const [file, setFile] = useState(null); // New state for the selected file
   const [ipfsHash, setIpfsHash] = useState("");
   const [searchTerm, setSearchTerm] = useState("");
   const [searchResult, setSearchResult] = useState("");
 
   const handleUpload = async () => {
-    // Call your backend/API to upload patent data and get the IPFS hash
-    // For simplicity, we're using a placeholder IPFS hash here
+    // Simple validation
+    if (!title.trim() || !description.trim() || !file) {
+      alert("Please fill in all fields.");
+      return;
+    }
+
+    // Perform file upload logic using the selected file
+    // Simulate file upload by displaying a placeholder IPFS hash
     const placeholderIpfsHash =
       "QmPZFdQ3vj3pYtYw9GJ9eYpa27pJ4tE8kZoEm5dQBb3ZoV";
     setIpfsHash(placeholderIpfsHash);
   };
 
   const handleSearch = async () => {
+    // Simple validation
+    if (!searchTerm.trim()) {
+      alert("Please enter a search term.");
+      return;
+    }
+
     // Call your backend/API to search for a patent based on the search term
     // For simplicity, we're using a placeholder search result here
     const placeholderSearchResult = "Patent Found!";
@@ -27,7 +39,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Patent Chain</h1>
+        <h1>Patentchain</h1>
         <div>
           <h2>Upload Patent</h2>
           <label>Title:</label>
@@ -42,6 +54,11 @@ function App() {
             value={description}
             onChange={(e) => setDescription(e.target.value)}
           />
+          <label>File:</label>
+          <input
+            type="file"
+            onChange={(e) => setFile(e.target.files[0])}
+          />
           <button onClick={handleUpload}>Upload</button>
           {ipfsHash && <p>IPFS Hash: {ipfsHash}</p>}
         </div>
@@ -49,13 +66,16 @@ function App() {
         <div>
           <h2>Search Patent</h2>
           <label>Search Term:</label>
-          <input
-            type="text"
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
-          />
-          <button onClick={handleSearch}>Search</button>
-          {searchResult && <p>Search Result: {searchResult}</p>}
+          <div className="search_div">
+            <input
+            className="search_input"
+              type="text"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+            />
+            <button className="search_button" onClick={handleSearch}>Search</button>
+            {searchResult && <p>Search Result: {searchResult}</p>}
+          </div>
         </div>
       </header>
     </div>
@@ -63,3 +83,4 @@ function App() {
 }
 
 export default App;
+
